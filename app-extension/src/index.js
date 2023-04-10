5b281c30c7aeb97de8e7ce4014a08c489dccc3b3
Min <solunar@vip.qq.com>
2023-04-10 15:10:28 +0800

    Update

--- app-extension/src/index.js
+++ app-extension/src/index.js
@@ -6,35 +6,34 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-function extendConf (conf) {
+function extendConf(conf, api) {
   // register our boot file
-  conf.boot.push('~@660e/quasar-app-extension-qcascader/src/boot/register.js')
+  conf.boot.push('~@660e/quasar-app-extension-qcascader/src/boot/register.js');
 
-  // make sure app extension files & ui package gets transpiled
-  conf.build.transpileDependencies.push(/@660e/quasar-app-extension-qcascader[\\/]src/)
+  if (!api.hasVite) {
+    // make sure app extension files & ui package gets transpiled
+    conf.build.transpileDependencies.push(/quasar-app-extension-qcascader[\\/]src/);
+  }
 
   // make sure the stylesheet goes through webpack to avoid SSR issues
-  conf.css.push('~@660e/quasar-ui-qcascader/src/index.sass')
+  conf.css.push('~@660e/quasar-ui-qcascader/src/index.sass');
 }
 
 module.exports = function (api) {
   // Quasar compatibility check; you may need
   // hard dependencies, as in a minimum version of the "quasar"
   // package or a minimum version of "@quasar/app-*" CLI
-  api.compatibleWith('quasar', '^2.0.0')
+  api.compatibleWith('quasar', '^2.0.0');
 
   if (api.hasVite) {
-    api.compatibleWith('@quasar/app-vite', '^1.0.0')
+    api.compatibleWith('@quasar/app-vite', '^1.0.0');
+  } else if (api.hasWebpack) {
+    api.compatibleWith('@quasar/app-webpack', '^3.4.0');
   }
-  else if (api.hasWebpack) {
-    api.compatibleWith('@quasar/app-webpack', '^3.4.0')
-  }
-
 
   // Uncomment the line below if you provide a JSON API for your component
   // api.registerDescribeApi('QCascader', '~@660e/quasar-ui-qcascader/src/components/QCascader.json')
 
-
   // We extend /quasar.conf.js
-  api.extendQuasarConf(extendConf)
-}
+  api.extendQuasarConf(extendConf);
+};
