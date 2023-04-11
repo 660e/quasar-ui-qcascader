f17adff7946f488a346e18f5233dcb7373c7632d
Min <solunar@vip.qq.com>
2023-04-11 15:56:14 +0800

    Update

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -1,5 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+const model = ref();
+const options = ref([
+  { id: 1, label: 'node-1' },
+  { id: 2, label: 'node-2' },
+  {
+    id: 3,
+    label: 'node-3',
+    children: [
+      { id: 31, label: 'node-3-1' },
+      { id: 32, label: 'node-3-2' },
+      {
+        id: 33,
+        label: 'node-3-3',
+        children: [
+          { id: 331, label: 'node-3-3-1' },
+          { id: 332, label: 'node-3-3-2' },
+          { id: 333, label: 'node-3-3-3' }
+        ]
+      }
+    ]
+  }
+]);
+</script>
+
 <template>
   <q-page padding>
-    <q-cascader />
+    <q-cascader v-model="model" :options="options" label="Outlined" dense outlined />
   </q-page>
 </template>
