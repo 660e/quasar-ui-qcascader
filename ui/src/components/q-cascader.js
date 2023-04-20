b7173c0ee815c51a710d6ae74007b214a4e16aff
Min <solunar@vip.qq.com>
2023-04-20 17:20:11 +0800

    Update

--- ui/src/components/q-cascader.js
+++ ui/src/components/q-cascader.js
@@ -1,13 +1,37 @@
 import { h } from 'vue';
-import { QBadge } from 'quasar';
+import { QItem, QItemSection, QList, QSelect } from 'quasar';
 
 export default {
   name: 'q-cascader',
 
-  setup() {
-    return () =>
-      h(QBadge, {
-        label: 'q-cascader'
+  setup(props, { attrs }) {
+    console.log(attrs.options);
+
+    const options = attrs.options;
+
+    function renderContainer() {
+      return h('div', { class: 'flex' }, renderList());
+    }
+
+    function renderList() {
+      return h(QList, null, { default: renderItem() });
+    }
+
+    function renderItem() {
+      return () => h(QItem, null, { default: renderItemSection() });
+    }
+
+    function renderItemSection() {
+      return () => h(QItemSection, null, () => 'option');
+    }
+
+    return () => {
+      const cascader = h(QSelect, null, {
+        'before-options': () => renderContainer(),
+        'option': () => h('div')
       });
+
+      return cascader;
+    };
   }
 };
