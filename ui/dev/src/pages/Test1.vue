a3da16b7ac4739e5cb812774d723f36067001792
Min <solunar@vip.qq.com>
2023-04-26 20:35:22 +0800

    Update

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -1,34 +1,47 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const model = ref();
-const options = ref([
-  { id: 1, label: 'node-1', children: [] },
+const options = reactive([
+  { id: 1, label: 'Node 1' },
   {
     id: 2,
-    label: 'node-2',
+    label: 'Node 2',
     children: [
-      { id: 21, label: 'node-2-1' },
-      { id: 22, label: 'node-2-2' },
-      { id: 23, label: 'node-2-3' },
-      { id: 24, label: 'node-2-4' }
+      { id: 21, label: 'Node 2.1' },
+      { id: 22, label: 'Node 2.2' },
+      {
+        id: 23,
+        label: 'Node 2.3',
+        children: [
+          { id: 231, label: 'Node 2.3.1' },
+          { id: 232, label: 'Node 2.3.2' }
+        ]
+      },
+      {
+        id: 24,
+        label: 'Node 2.4',
+        children: [
+          {
+            id: 241,
+            label: 'Node 2.4.1',
+            children: [
+              { id: 2411, label: 'Node 2.4.1.1' },
+              { id: 2412, label: 'Node 2.4.1.2' },
+              { id: 2413, label: 'Node 2.4.1.3' }
+            ]
+          },
+          { id: 242, label: 'Node 2.4.2' }
+        ]
+      }
     ]
   },
   {
     id: 3,
-    label: 'node-3',
+    label: 'Node 3',
     children: [
-      { id: 31, label: 'node-3-1' },
-      { id: 32, label: 'node-3-2' },
-      {
-        id: 33,
-        label: 'node-3-3',
-        children: [
-          { id: 331, label: 'node-3-3-1' },
-          { id: 332, label: 'node-3-3-2' },
-          { id: 333, label: 'node-3-3-3' }
-        ]
-      }
+      { id: 31, label: 'Node 3.1' },
+      { id: 32, label: 'Node 3.2' }
     ]
   }
 ]);
@@ -36,7 +49,9 @@ const options = ref([
 
 <template>
   <q-page padding>
-    <q-cascader v-model="model" :options="options" label="Outlined" dense options-dense outlined />
+    <div style="width: 400px">
+      <q-cascader v-model="model" :options="options" label="Outlined" dense options-dense outlined />
+    </div>
     <pre class="q-my-md">{{ model }}</pre>
   </q-page>
 </template>
